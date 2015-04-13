27ac7503f0ea5569cb329d7079446485ae3a342f
SimoneS93 <salernosimone.uni@gmail.com>
2015-04-13 16:04:34 +0200

    Create single_timer.c

---
+++ EVERY/examples/single_timer.c
@@ -0,0 +1,22 @@
+/**
+ * EVERY lib example: exec a piece of code every N millis
+ */
+ 
+#include "EVERY.h"
+
+void setup() {
+  Serial.begin(9600);
+  EVERY_INIT();
+}
+
+void loop() {  
+  /*
+   * @param index The timer index, from 0 to 9 (by default)
+   * @param interval The timer interval
+   */
+  EVERY(0, 3000) {
+    Serial.println("3 seconds have passed");
+  }
+  
+  delay(1);
+}
